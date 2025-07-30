e348717d23b33929fe2459deb3b5503537161ef3
Apurbo Deb Nath <apurbodebnath50@gmail.com>
2025-07-30 12:49:31 +0600

    concatenate

--- a.py
+++ a.py
@@ -16,7 +16,7 @@ sed do eiusmod tempor incididunt
 ut labore et dolore magna aliqua."""
 print(a)
 
-# Python - Slicing Strings *******
+#                                                 Python - Slicing Strings *******
 # You can return a range of characters by using the slice syntax.
 
 # Specify the start index and the end index, separated by a colon, to return a part of the string.
@@ -28,7 +28,7 @@ print(b[2:5])
 # Output: llo
 
 
-# Slice From the Start *****
+#                                                Slice From the Start *****
 # By leaving out the start index, the range will start at the first character:
 
 # Get the characters from the start to position 5 (not included):
@@ -38,7 +38,7 @@ print(b[:5])
 
 # Output: Hello
 
-#Slice To the End
+#                                                           Slice To the End
 # By leaving out the end index, the range will go to the end:
 
 Get the characters from position 2, and all the way to the end:
@@ -49,7 +49,7 @@ print(b[2:])
 #Output: llo, World!
 
 
-# Negative Indexing ********
+#                                                   Negative Indexing ********
 # Use negative indexes to start the slice from the end of the string:
 # Example
 # Get the characters:
@@ -62,8 +62,8 @@ b = "Hello, World!"
 print(b[-5:-2])
 #Output: orl
 
-
-# Upper Case **********
+ 
+#                                                   Upper Case **********
 # ExampleGet your own Python Server
 # The upper() method returns the string in upper case:
 
@@ -72,7 +72,7 @@ print(a.upper())
 
 # Output: HELLO, WORLD!
 
-# Lower Case *******
+#                                                      Lower Case *******
 # Example
 # The lower() method returns the string in lower case:
 
@@ -81,7 +81,7 @@ print(a.lower())
 # Output: hello, world!
 
 
-# Remove Whitespace *****
+#                                                Remove Whitespace *****
 # Whitespace is the space before and/or after the actual text, and very often you want to remove this space.
 
 # Example
@@ -93,7 +93,7 @@ print(a.strip())
 # returns Hello, World!
 
 
-# Replace String
+#                                                      Replace String
 # Example
 # The replace() method replaces a string with another string:
 
@@ -104,7 +104,7 @@ print(a.replace("H", "J"))
 
 
 
-# Split String *****
+#                                                Split String *****
 # The split() method returns a list where the text between the specified separator becomes the list items.
 
 # Example
@@ -114,3 +114,27 @@ a = "Hello, World!"
 print(a.split(",")) 
 
 # returns ['Hello', ' World!']
+
+
+#                                           String Concatenation ***********
+
+
+# To concatenate, or combine, two strings you can use the + operator.
+
+# ExampleGet your own Python Server
+# Merge variable a with variable b into variable c:
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+
+
+# Example
+# To add a space between them, add a " ":
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)

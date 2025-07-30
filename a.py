5406a3a3c82c71b12f6873b80129480f0ad58df1
Apurbo Deb Nath <apurbodebnath50@gmail.com>
2025-07-30 12:25:24 +0600

    slicing

--- a.py
+++ a.py
@@ -26,3 +26,14 @@ b = "Hello, World!"
 print(b[2:5])
 
 # Output: llo
+
+
+# Slice From the Start *****
+# By leaving out the start index, the range will start at the first character:
+
+# Get the characters from the start to position 5 (not included):
+
+b = "Hello, World!"
+print(b[:5])
+
+# Output: Hello

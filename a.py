a21b1a32fe3838cc53bd19c10946f9548b2a3d9e
Apurbo Deb Nath <apurbodebnath50@gmail.com>
2025-07-30 12:44:53 +0600

    Split()

--- a.py
+++ a.py
@@ -100,3 +100,17 @@ print(a.strip())
 a = "Hello, World!"
 print(a.replace("H", "J"))
 
+#  Jello, World!
+
+
+
+# Split String *****
+# The split() method returns a list where the text between the specified separator becomes the list items.
+
+# Example
+# The split() method splits the string into substrings if it finds instances of the separator:
+
+a = "Hello, World!"
+print(a.split(",")) 
+
+# returns ['Hello', ' World!']

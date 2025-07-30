e9bdbedce9f37b2c9e95f3be64305ac8b7aea01a
Apurbo Deb Nath <apurbodebnath50@gmail.com>
2025-07-30 12:41:02 +0600

    done

--- a.py
+++ a.py
@@ -72,3 +72,31 @@ print(a.upper())
 
 # Output: HELLO, WORLD!
 
+# Lower Case *******
+# Example
+# The lower() method returns the string in lower case:
+
+a = "Hello, World!"
+print(a.lower())
+# Output: hello, world!
+
+
+# Remove Whitespace *****
+# Whitespace is the space before and/or after the actual text, and very often you want to remove this space.
+
+# Example
+# The strip() method removes any whitespace from the beginning or the end:
+
+a = " Hello, World! "
+print(a.strip()) 
+
+# returns Hello, World!
+
+
+# Replace String
+# Example
+# The replace() method replaces a string with another string:
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+

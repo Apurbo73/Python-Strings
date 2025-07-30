07bde7b1e15e0384d73d475014e42df0777fbad7
Apurbo Deb Nath <apurbodebnath50@gmail.com>
2025-07-30 12:30:17 +0600

    done

--- a.py
+++ a.py
@@ -47,3 +47,17 @@ b = "Hello, World!"
 print(b[2:])
 
 #Output: llo, World!
+
+
+# Negative Indexing ********
+# Use negative indexes to start the slice from the end of the string:
+# Example
+# Get the characters:
+
+# From: "o" in "World!" (position -5)
+
+# To, but not included: "d" in "World!" (position -2):
+
+b = "Hello, World!"
+print(b[-5:-2])
+#Output: orl
